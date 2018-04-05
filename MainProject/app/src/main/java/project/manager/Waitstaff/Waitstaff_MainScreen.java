6ba2027e7522e5e5580afc099d9245025febd90b
Trenton Blackburn <trent_blackburn97@yahoo.com>
2018-04-05 18:57:53 -0500

    Revert "added"
    This reverts commit b04240b5e9e66d879927b9a798aa4ad4387079dd.

--- MainProject/app/src/main/java/project/manager/Waitstaff/Waitstaff_MainScreen.java
+++ MainProject/app/src/main/java/project/manager/Waitstaff/Waitstaff_MainScreen.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -35,9 +34,13 @@ public class Waitstaff_MainScreen extends AppCompatActivity {
                 EditText waiterPassword;
                 waiterPassword = (EditText) findViewById(R.id.waitstaff_password);
 
+<<<<<<< HEAD
                 //take this out lmao
                 Log.d("log: ", " LOGGED LMAO ");
 
+=======
+                /*
+>>>>>>> parent of b04240b... added
                 //if waiters aren't fully staffed and user isn't already "clocked in", "clock in" waiter
                 if(myDb.doesWaiterExist(waiterName.getText().toString()) == true) {
                     Toast.makeText(getApplicationContext(), ("Welcome back, " + waiterName.getText().toString()), Toast.LENGTH_SHORT).show();
@@ -45,13 +48,25 @@ public class Waitstaff_MainScreen extends AppCompatActivity {
                 }
                 else if(myDb.getWaiterCount() < 4){
                     myDb.WSlogin(waiterName.getText().toString(), waiterPassword.getText().toString());
+<<<<<<< HEAD
                     Toast.makeText(getApplicationContext(), (waiterName.getText().toString() + " clocked in!."), Toast.LENGTH_SHORT).show();
                     staff_sign_in();
                 }
                 else {
                     Toast.makeText(getApplicationContext(), "There are 4 waiters clocked in already.", Toast.LENGTH_SHORT).show();
+=======
+>>>>>>> parent of b04240b... added
                 }
+                */
 
+<<<<<<< HEAD
+=======
+                Toast.makeText(getApplicationContext(), (waiterName.getText().toString() + " signed in."), Toast.LENGTH_SHORT).show();
+
+                //get password?
+
+                staff_sign_in();
+>>>>>>> parent of b04240b... added
             }
         });
     }
