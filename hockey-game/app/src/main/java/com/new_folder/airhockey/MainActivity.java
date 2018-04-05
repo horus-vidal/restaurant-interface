16af1796a97e3df2fac938a62290360c908f18c7
Chimchilla55 <chimaakanno@my.unt.edu>
2018-04-05 18:41:37 -0500

    Revert "Tic Tac Toe"
    This reverts commit 7f0d5ca830ca5a8f7bb04ce821728b58516753b6.

--- hockey-game/app/src/main/java/com/new_folder/airhockey/MainActivity.java
+++ hockey-game/app/src/main/java/com/new_folder/airhockey/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_air_hockey);
+        setContentView(R.layout.activity_main);
     }
 
     public void sendMessage(View view) {
