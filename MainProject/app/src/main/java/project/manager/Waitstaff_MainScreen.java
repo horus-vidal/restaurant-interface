4ab50b351c9aee90e47283ea4b254fb20ef6b3b2
Trenton Blackburn <trent_blackburn97@yahoo.com>
2018-04-04 20:55:00 -0500

    chima's xml inclusions

--- MainProject/app/src/main/java/project/manager/Waitstaff_MainScreen.java
+++ MainProject/app/src/main/java/project/manager/Waitstaff_MainScreen.java
@@ -13,6 +13,7 @@ public class Waitstaff_MainScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //creates and preps button to be clicked
         Button sign_in_button = (Button) findViewById(R.id.log_in_button);
         sign_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,6 +22,8 @@ public class Waitstaff_MainScreen extends AppCompatActivity {
             }
         });
     }
+
+    //sends user to waiter main menu if user clicks login
     public void staff_sign_in() {
         Intent signIn_intent = new Intent(this, Waitstaff_TablesActivity.class);
         startActivity(signIn_intent);
